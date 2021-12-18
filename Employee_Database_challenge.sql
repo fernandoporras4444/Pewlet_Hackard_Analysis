fecf3cd7a0a235a27c1fa2bc8e52826e326962a2
fernando <fernandoporras4444@yahoo.com>
2021-12-18 14:19:05 -0600

    Updated mentorship eligibility sql code

--- Employee_Database_challenge.sql
+++ Employee_Database_challenge.sql
@@ -63,3 +63,27 @@ ORDER BY COUNT(title) DESC;
 
 ----Verify retiring_titles TABLE
 SELECT * FROM retiring_titles;
+
+---Create mentorship_eligibility TABLE with birth_date '1965'
+SELECT
+DISTINCT ON (emp.emp_no)
+emp.emp_no,
+emp.first_name,
+emp.last_name,
+emp.birth_date,
+de.from_date,
+de.to_date,
+ti.title
+
+INTO mentorship_eligibility
+
+FROM employees AS emp
+LEFT JOIN dept_emp AS de ON emp.emp_no = de.emp_no
+LEFT JOIN titles AS ti ON emp.emp_no = ti.emp_no
+
+WHERE emp.birth_date BETWEEN '1965-01-01' AND '1965-12-31'
+
+ORDER BY emp.emp_no;
+
+----Verify mentorship_eligibility TABLE
+SELECT * FROM mentorship_eligibility;
